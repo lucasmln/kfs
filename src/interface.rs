52050ea685ab55b58448b8a1872475eac42a9ed6
avan-pra <avan-pra@users.noreply.github.com>
2023-09-29 17:22:59 +0200

    Add utility to print to the screen

--- src/interface.rs
+++ src/interface.rs
@@ -1,4 +1,4 @@
-pub const VGA_ADDRESS: u32 = 0xB8000;
+const VGA_ADDRESS: u32 = 0xB8000;
 const WIDTH: u32 = 80;
 const HEIGHT: u32 = 25;
 
@@ -31,49 +31,75 @@ pub struct Interface {
     pub cursor: u32,
     pub vga_address: *mut Cell,
 }
-
-pub unsafe fn print_at_offset(x: u32, y: u32, interface: &Interface, cell: &Cell) {
-    assert!(x < WIDTH);
-    assert!(y < HEIGHT);
-    (*interface.vga_address.offset((x + (HEIGHT * y)) as isize)).character = cell.character;
-    (*interface.vga_address.offset((x + (HEIGHT * y)) as isize)).color = cell.color;
+impl Default for Interface {
+    fn default() -> Self {
+        Self { cursor: 0, vga_address: VGA_ADDRESS as *mut Cell}
+    }
 }
+impl Interface {
 
-pub unsafe fn print_cell(interface: &mut Interface, cell: &Cell) {
-    (*interface.vga_address.offset(interface.cursor as isize)).character = cell.character;
-    (*interface.vga_address.offset(interface.cursor as isize)).color = cell.color;
-    interface.cursor += 1;
-}
+    pub unsafe fn print_cell_at_offset(&mut self, x: u32, y: u32, cell: &Cell) {
+        assert!(x < WIDTH);
+        assert!(y < HEIGHT);
+        (*self.vga_address.offset((x + (WIDTH * y)) as isize)).character = cell.character;
+        (*self.vga_address.offset((x + (WIDTH * y)) as isize)).color = cell.color;
+    }
 
-pub unsafe fn print_char(interface: &mut Interface, character: &u8, color: &Colors) {
-    (*interface.vga_address.offset(interface.cursor as isize)).character = *character;
-    (*interface.vga_address.offset(interface.cursor as isize)).color = *color as u8;
-    interface.cursor += 1;
-}
+    pub unsafe fn print_char_at_offset(&mut self, x: u32, y: u32, character: &u8, color: &Colors) {
+        let cell = Cell { character: *character, color: *color as u8 };
+        self.print_cell_at_offset(x, y, &cell)
+    }
 
-pub fn set_cursor(x: u32, y: u32, interface: &mut Interface) {
-    assert!(x < WIDTH);
-    assert!(y < HEIGHT);
-    interface.cursor = x + (HEIGHT * y)
-}
-
-pub unsafe fn print_string(interface: &mut Interface, str: &[u8], color: &Colors) {
-    for (_, byte) in str.iter().enumerate() {
-        if *byte == b'\n' {
-            interface.cursor = interface.cursor + WIDTH - interface.cursor % WIDTH
+    pub unsafe fn print_cell(&mut self, cell: &Cell) {
+        if self.cursor >= HEIGHT * WIDTH {
+            for i in 0..HEIGHT * WIDTH - WIDTH {
+                (*self.vga_address.offset(i as isize)).character = (*self.vga_address.offset((i + WIDTH) as isize)).character;
+                (*self.vga_address.offset(i as isize)).color = (*self.vga_address.offset((i + WIDTH) as isize)).color;
+            }
+            self.clear_line(HEIGHT - 1);
+            self.cursor = HEIGHT * WIDTH - WIDTH;
+        }
+        if cell.character == b'\n' {
+            self.cursor = self.cursor + WIDTH - self.cursor % WIDTH;
         }
         else {
-            print_char(interface, byte, color)
+            (*self.vga_address.offset(self.cursor as isize)).character = cell.character;
+            (*self.vga_address.offset(self.cursor as isize)).color = cell.color;
+            self.cursor += 1;
         }
     }
-}
 
-pub unsafe fn clear_screen(interface: &mut Interface) {
-    let cell: Cell = Cell { character: 0, color: 0 };
-
-    interface.cursor = 0;
-    for _ in 0..WIDTH * HEIGHT { 
-        print_cell(interface, &cell);
+    pub unsafe fn print_char(&mut self, character: &u8, color: &Colors) {
+        let cell = Cell { character: *character, color: *color as u8 };
+        self.print_cell(&cell);
     }
-    interface.cursor = 0;
+
+    pub unsafe fn print_string(&mut self, str: &[u8], color: &Colors) {
+        for (_, byte) in str.iter().enumerate() {
+            self.print_char(byte, color)
+        }
+    }
+
+    pub fn set_cursor(&mut self, x: u32, y: u32) {
+        assert!(x < WIDTH);
+        assert!(y < HEIGHT);
+        self.cursor = x + (HEIGHT * y)
+    }
+
+    pub unsafe fn clear_line(&mut self, n: u32) {
+        let cell: Cell = Cell { character: 0, color: 0 };
+        for x in 0..WIDTH { 
+            self.print_cell_at_offset(x, n, &cell);
+        }
+    }
+
+    pub unsafe fn clear_screen(&mut self) {
+        let cell: Cell = Cell { character: 0, color: 0 };
+
+        self.cursor = 0;
+        for _ in 0..WIDTH * HEIGHT { 
+            self.print_cell(&cell);
+        }
+        self.cursor = 0;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,25 @@
 
 mod interface;
 
-use interface::Cell;
 use interface::Interface;
 use interface::Colors;
-use interface::print_string;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn main() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
 
-    let mut interface = Interface { cursor: 0, vga_address: interface::VGA_ADDRESS as *mut Cell};
+    let mut interface = Interface::default();
 
     unsafe {
-        interface::clear_screen(&mut interface);
-        let str: &[u8] = b"salut\ncava ?";
-        print_string(&mut interface, str, &Colors::Green);
+        interface.clear_screen();
+        let str: &[u8] = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nb\nc\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n";
+        interface.print_string(str, &Colors::Green);
+        interface.print_string(b"salut la \nteam", &Colors::BrightBlue);
+        core::arch::asm!(
+            "hlt"
+        )
     }
-    // clear_screan(vga_buffer);
     loop {}
 }
 
