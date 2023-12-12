a2f6a7d62746e29fcae229143ac8d30911209030
lmoulin <lucas.moulin78114@gmail.com>
2023-12-12 16:15:59 +0100

    * add a stack to save active and pressed keys * Save current kb in kb_state * Enable switching kb from fr to en

--- src/keyboard/mod.rs
+++ src/keyboard/mod.rs
@@ -1,61 +1,126 @@
 pub mod en;
-pub mod apple_fr;
+pub mod fr;
 
 use crate::{print, println};
 use crate::io::{outb, inb};
 
-pub struct KeyboardState {
+
+pub struct Stack {
+    data: [u8; 10]
+}
+
+impl Default for Stack {
+    fn default() -> Self {
+        Self { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+    }
+}
+
+impl Stack {
+    fn push(&mut self, elem: u8) {
+        for i in (1..10).rev() {
+            self.data[i] = self.data[i - 1];
+        }
+        self.data[0] = elem;
+    }
+
+    fn pop(&mut self) {
+        for i in 0..9 {
+            self.data[i] = self.data[i + 1];
+        }
+        self.data[9] = 0;
+    }
+
+    fn has(&mut self, elem: u8) -> bool {
+        for i in 0..10 {
+            if self.data[i] == elem {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+pub struct Kmap<'a> {
+    map: &'a[&'a str; 69],
+    map_shift: &'a[&'a str; 69]
+}
+
+pub struct KeyboardState<'a> {
     lshift: bool,
     rshift: bool,
     lctrl: bool,
     loption: bool,
-    roption: bool,
     lcmd: bool,
     rcmd: bool,
     fn_btn: bool,
     caps_lock: bool,
+    lang: [Kmap<'a>; 2],
+    lang_id: usize,
+    pressed_key: Stack,
+    active_key: Stack
 }
 
-impl Default for KeyboardState {
+impl Default for KeyboardState<'_> {
     fn default() -> Self {
         Self {
             lshift: false,
             rshift: false,
             lctrl: false,
             loption: false,
-            roption: false,
             lcmd: false,
             rcmd: false,
             fn_btn: false,
-            caps_lock: false
+            caps_lock: false,
+            lang: [
+                Kmap {
+                    map: &fr::KMAP, map_shift: &fr::KMAP_SHIFT
+                },
+                Kmap {
+                    map: &en::KMAP, map_shift: &en::KMAP_SHIFT
+                }
+            ],
+            lang_id: 0,
+            pressed_key: Stack::default(),
+            active_key: Stack::default()
         }
     }
 }
 
 use lazy_static::lazy_static;
-use spin::Mutex;
+use spin::{Mutex, MutexGuard};
+
 lazy_static! {
-    static ref KEYBOARD_STATE: Mutex<KeyboardState> = Mutex::new(KeyboardState::default());
+    static ref KEYBOARD_STATE: Mutex<KeyboardState<'static>> = Mutex::new(KeyboardState::default());
+}
+
+fn is_switch_kb(knbr: u8, kb_state: &MutexGuard<'_, KeyboardState<'_>>) -> bool {
+    if knbr == fr::Kvalue::Space as u8 && kb_state.loption && kb_state.active_key.data[1] == 0 {
+        return true;
+    }
+    return false;
 }
 
 fn key_unpress(knbr: u8) {
     let unpressed_knbr = knbr - 128;
-    match crate::keyboard::apple_fr::KMAP.get(unpressed_knbr as usize) {
-        Some(key) => {
-            let mut state = KEYBOARD_STATE.lock();
-            if knbr == apple_fr::Kvalue::Ctrl as u8 {
+    let mut state = KEYBOARD_STATE.lock();
+
+    match state.lang[state.lang_id].map.get(unpressed_knbr as usize) {
+        Some(_key) => {
+            if unpressed_knbr == fr::Kvalue::Ctrl as u8 {
                 state.lctrl = false;
-            } else if knbr == apple_fr::Kvalue::LShift as u8 {
+            } else if unpressed_knbr == fr::Kvalue::LShift as u8 {
                 state.lshift = false;
-            } else if knbr == apple_fr::Kvalue::Rshift as u8  {
+            } else if unpressed_knbr == fr::Kvalue::Rshift as u8  {
                 state.rshift = false;
-            } else if knbr == apple_fr::Kvalue::Option as u8  {
+            } else if unpressed_knbr == fr::Kvalue::Option as u8  {
                 // loption and roption have the same value in macos keyboard
                 state.loption = false;
+            } else {
+                state.pressed_key.push(knbr);
             }
+            state.active_key.pop();
         }
         None => {
-            let mut state = KEYBOARD_STATE.lock();
             if unpressed_knbr == 91 {
                 state.lcmd = false;
             } else if unpressed_knbr == 92 {
@@ -63,36 +128,44 @@ fn key_unpress(knbr: u8) {
             } else {
                 println!("None : {}", unpressed_knbr);
             }
+            state.active_key.pop();
         }
     }
 }
 
 fn key_press(knbr: u8) {
-    let mut map = crate::keyboard::apple_fr::KMAP;
     let mut state = KEYBOARD_STATE.lock();
+    let map;
     
     if state.lshift == true || state.rshift == true || state.caps_lock == true {
-        map = crate::keyboard::apple_fr::KMAP_SHIFT;
+        map = state.lang[state.lang_id].map_shift;
+    } else {
+        map = state.lang[state.lang_id].map;
     }
     match map.get(knbr as usize) {
         Some(key) => {
-            if knbr == apple_fr::Kvalue::Ctrl as u8 {
+            if knbr == fr::Kvalue::Ctrl as u8 {
                 state.lctrl = true;
-            } else if knbr == apple_fr::Kvalue::LShift as u8 {
+            } else if knbr == fr::Kvalue::LShift as u8 {
                 state.lshift = true;
-            } else if knbr == apple_fr::Kvalue::Rshift as u8  {
+            } else if knbr == fr::Kvalue::Rshift as u8  {
                 state.rshift = true;
-            } else if knbr == apple_fr::Kvalue::Option as u8  {
+            } else if knbr == fr::Kvalue::Option as u8  {
                 // loption and roption have the same value in macos keyboard
                 state.loption = true;
-            } else if knbr == apple_fr::Kvalue::ShiftLock as u8 {
+            } else if knbr == fr::Kvalue::ShiftLock as u8 {
                 state.caps_lock = !state.caps_lock;
-            } else {
-                println!("{} {}", key, knbr);
+            } else if is_switch_kb(knbr, &state) {
+                state.lang_id = (state.lang_id + 1) % 2;
+            }
+            else {
+                print!("{}", key);
+            }
+            if state.active_key.has(knbr) == false {
+                state.active_key.push(knbr);
             }
         }
         None => {
-            let mut state = KEYBOARD_STATE.lock();
             if knbr == 91 {
                 state.lcmd = true;
             } else if knbr == 92 {
@@ -100,6 +173,7 @@ fn key_press(knbr: u8) {
             } else if knbr < 128 {
                 println!("nbr: {}", knbr);
             }
+            state.active_key.push(knbr);
         }
     }
 }
