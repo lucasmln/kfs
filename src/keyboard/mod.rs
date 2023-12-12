80739e5bd085b8c2c7cdf589a0c354d2c164ff0e
lmoulin <lucas.moulin78114@gmail.com>
2023-12-12 18:23:24 +0100

    remove debug code and send only printable key

--- src/keyboard/mod.rs
+++ src/keyboard/mod.rs
@@ -101,6 +101,29 @@ fn is_switch_kb(knbr: u8, kb_state: &MutexGuard<'_, KeyboardState<'_>>) -> bool 
     return false;
 }
 
+fn is_printable(value: fr::Kvalue) -> bool {
+    match value {
+        fr::Kvalue::Null => { false }
+        fr::Kvalue::Esc => { false }
+        fr::Kvalue::Ctrl => { false }
+        fr::Kvalue::LShift => { false }
+        fr::Kvalue::Rshift => { false }
+        fr::Kvalue::Option => { false }
+        fr::Kvalue::ShiftLock => { false }
+        fr::Kvalue::F1 => { false }
+        fr::Kvalue::F2 => { false }
+        fr::Kvalue::F3 => { false }
+        fr::Kvalue::F4 => { false }
+        fr::Kvalue::F5 => { false }
+        fr::Kvalue::F6 => { false }
+        fr::Kvalue::F7 => { false }
+        fr::Kvalue::F8 => { false }
+        fr::Kvalue::F9 => { false }
+        fr::Kvalue::F10 => { false }
+        _ => { true }
+    }
+}
+
 fn key_unpress(knbr: u8) {
     let unpressed_knbr = knbr - 128;
     let mut state = KEYBOARD_STATE.lock();
@@ -126,8 +149,6 @@ fn key_unpress(knbr: u8) {
                 state.lcmd = false;
             } else if unpressed_knbr == 92 {
                 state.rcmd = false;
-            } else {
-                println!("None : {}", unpressed_knbr);
             }
             state.active_key.pop();
         }
@@ -145,7 +166,8 @@ fn key_press(knbr: u8) {
     }
     match map.get(knbr as usize) {
         Some(key) => {
-            match fr::get_kvalue(knbr) {
+            let kvalue = fr::get_kvalue(knbr);
+            match kvalue {
                 fr::Kvalue::Ctrl => {
                     state.lctrl = true;
                 }
@@ -169,7 +191,9 @@ fn key_press(knbr: u8) {
                     if is_switch_kb(knbr, &state) {
                         state.lang_id = (state.lang_id + 1) % 2;
                     } else {
-                        shell::read(key.as_bytes());
+                        if is_printable(kvalue) {
+                            shell::read(key.as_bytes());
+                        }
                     }
                 }
             }
@@ -182,8 +206,6 @@ fn key_press(knbr: u8) {
                 state.lcmd = true;
             } else if knbr == 92 {
                 state.rcmd = true;
-            } else if knbr < 128 {
-                println!("nbr: {}", knbr);
             }
             state.active_key.push(knbr);
         }
